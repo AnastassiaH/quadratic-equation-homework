48a8dfb18f71f55163b21e58c5cf27af61ee9234
Anastasiia Hurska <nastyagurska@gmail.com>
2018-10-28 21:22:36 +0200

    home-work-fibonacci

--- js/index.js
+++ js/index.js
@@ -1,45 +1,40 @@
-// я використала "var" там де потрібно було щоб змінні були доступні за межами блоку в якому вони були визначені
-// спочатку тут було не багато коду але почали вилазити помилки з від'ємним дискримінантом і так получилося
-// я знаю що виконала завдання не зовсім так як ви просили, але я хотіла щось трошки складніше попрактикувати
-
 button.onclick = function(){
-    var d; // discriminator
-    var solution = document.getElementById("solution"); // variable for paragraph with answer
-        
     
-function getResults(a, b, c){
-    d = b * b - 4 * a * c;
-    if(d >= 0){
-        console.log(-b);
-        var x1 = (-b + Math.sqrt(d)) / (2 * a); 
-        var x2 = (b - Math.sqrt(d)) / (2 * a);
-    } 
+    var number = parseFloat(document.getElementById("num").value);   
 
-    let resultArr = [x1, x2];
-    return resultArr;
+    document.getElementById("solutionByLoop").innerHTML = "Solution by array " + gettingNumberByArray(number);
+    document.getElementById("solutionByArray").innerHTML = "Solution by array " + gettingNumberByArray(number);
+    document.getElementById("solutionByRecursion").innerHTML = "Solution by recursion " + gettingNumberByRecursion(number); 
+
+    document.getElementById("answer").style.display = "block";
 }
-    
 
-// getting values of variables from user
-let a = document.getElementById("a").value;
-let b = document.getElementById("b").value;
-let c = document.getElementById("c").value;
-    
-// showing quadratic equation
-document.getElementById("equation").innerHTML = a + "x<sup>2</sup> " + "+ " + b + "x + " + c + " = 0";
-
-// call function
-getResults(a,b,c);
-
-    
-// answer when discriminator is positive or negative;
-    if (d >= 0){
-        solution.innerHTML = "Solution one is " + getResults(a,b,c)[0] + "<br>Solution two is " + getResults(a,b,c)[1];
+function gettingNumberByLoop(num) {  
+    let first = 1;
+    let second = 1;
+    let next = 0;
+    for (let i = 0; i < num-2; i++){
+        next = first + second;
+        first = second;
+        second = next;
     }
-    else {
-        solution.textContent = "The discriminator is negative";
-    }
+    return next;
+}
 
-// show answer block
-document.getElementById("answer").style.display = "block";
+function gettingNumberByArray(num){
+    let fibArr = [1, 1];
+    for (let i = 2; i < num; i++){
+        fibArr.push(fibArr[i-1] + fibArr[i-2]);
+    }
+    return fibArr.pop();
+}
+
+function gettingNumberByRecursion(num){
+//    (num === 1 || num === 2) ? return 1;
+    if(num === 1 || num === 2){
+        return 1;
+    }
+    for (let i = 0; i < num-2; i++){
+        return gettingNumberByRecursion(num-1) + gettingNumberByRecursion(num-2);
+    }
 }
